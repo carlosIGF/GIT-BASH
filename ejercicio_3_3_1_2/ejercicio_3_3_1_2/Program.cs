16eff36ca514ae2bccd6c64fda3d5ae9b0c556e9
CarlosIGF <carlos.igformacion@gmail.com>
2020-11-13 09:23:02 +0100

    EJERCICIO 3.3.1.2

---
+++ ejercicio_3_3_1_2/ejercicio_3_3_1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicio_3_3_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra;
+
+            Console.Write("Escribe una letra: ");
+            letra = Convert.ToChar(Console.ReadLine());
+
+            for (char c = letra; c <= 'z'; c++, c++)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
